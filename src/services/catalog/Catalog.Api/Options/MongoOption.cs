0b8952b84ee5c740da44953a4f41944ae59cd160
ozudev <ozudev@gmail.com>
2025-07-02 00:34:47 +0300

    3.Mongodb connectionstring kismi eklendi

---
+++ src/services/catalog/Catalog.Api/Options/MongoOption.cs
@@ -0,0 +1,9 @@
+﻿namespace Catalog.Api.Options;
+
+public class MongoOption
+{
+    public string DatabaseName { get; set; } = null!;
+    public string ConnectionString { get; set; } = null!;
+}
+
+// Database bağlamak için yardımcı bir sınıf OptionExtensions içerisinde ekledik.
