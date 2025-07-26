0c738089891a3fbd5090764323d26609fce6c9c2
ozudev <ozudev@gmail.com>
2025-07-27 00:37:25 +0300

    2.52.Command ve Validator kismini yazdik

---
+++ src/services/basket/Microservice.Basket.Api/Features/Baskets/AddBasketItem/AddBasketItemCommand.cs
@@ -0,0 +1,3 @@
+﻿namespace Microservice.Basket.Api.Features.Baskets.AddBasketItem;
+
+public record AddBasketItemCommand(Guid Id, string CourseName, decimal CoursePrice, string? ImageUrl);
---
+++ src/services/basket/Microservice.Basket.Api/Features/Baskets/AddBasketItem/AddBasketItemCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+
+namespace Microservice.Basket.Api.Features.Baskets.AddBasketItem;
+
+public class AddBasketItemCommandValidator : AbstractValidator<AddBasketItemCommand>
+{
+    public AddBasketItemCommandValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty()
+            .WithMessage("Id is required.");
+        RuleFor(x => x.CourseName)
+            .NotEmpty()
+            .WithMessage("Course name is required.")
+            .MaximumLength(100)
+            .WithMessage("Course name must not exceed 100 characters.");
+        RuleFor(x => x.CoursePrice)
+            .GreaterThan(0)
+            .WithMessage("Course price must be greater than zero.");
+    }
+}
