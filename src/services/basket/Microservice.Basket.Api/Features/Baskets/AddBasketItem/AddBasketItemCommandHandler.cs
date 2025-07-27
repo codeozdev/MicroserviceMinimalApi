39be7cae79882fae94fbe73f486cefe3d0725d93
ozudev <ozudev@gmail.com>
2025-07-27 21:13:36 +0300

    4.54.var olan kodu daha sadelestiriyor fakat bunu ben tam olarak yapmadim okumayi daha zorlastirdigini dusundugum icin

--- src/services/basket/Microservice.Basket.Api/Features/Baskets/AddBasketItem/AddBasketItemCommandHandler.cs
+++ src/services/basket/Microservice.Basket.Api/Features/Baskets/AddBasketItem/AddBasketItemCommandHandler.cs
@@ -34,17 +34,15 @@ public class AddBasketItemCommandHandler(IDistributedCache distributedCache) : I
         {
             currentBasket = JsonSerializer.Deserialize<BasketDto>(basketAsString); // Var olan sepeti JSON'dan nesye dönüştür
 
-            var existingItem = currentBasket.BasketItems.FirstOrDefault(x => x.Id == request.Id); // Sepette aynı ürün var mı kontrol et
+            var existingItem = currentBasket!.BasketItems.FirstOrDefault(x => x.Id == request.Id); // Sepette aynı ürün var mı kontrol et
 
             if (existingItem is not null) // Eğer aynı ürün varsa önce sil, sonra güncel haliyle ekle
             {
                 currentBasket.BasketItems.Remove(existingItem);
-                currentBasket.BasketItems.Add(newBasketItem);
             }
-            else // Aynı ürün yoksa doğrudan ekle
-            {
-                currentBasket.BasketItems.Add(newBasketItem);
-            }
+
+            // Aynı ürün yoksa doğrudan ekle
+            currentBasket.BasketItems.Add(newBasketItem);
         }
 
         basketAsString = JsonSerializer.Serialize(currentBasket); // Sepeti tekrar JSON'a çevir
