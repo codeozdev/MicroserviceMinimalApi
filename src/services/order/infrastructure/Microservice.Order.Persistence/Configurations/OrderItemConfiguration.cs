2ae05e05aa7f5832b9c87e1cddcb37a69fd72cbb
ozudev <ozudev@gmail.com>
2025-08-01 02:26:12 +0300

    75. Entity siniflarimizin configuration siniflari yapildi

---
+++ src/services/order/infrastructure/Microservice.Order.Persistence/Configurations/OrderItemConfiguration.cs
@@ -0,0 +1,17 @@
+using Microservice.Order.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Microservice.Order.Persistence.Configurations;
+
+public class OrderItemConfiguration : IEntityTypeConfiguration<Domain.Entities.OrderItem>
+{
+    public void Configure(EntityTypeBuilder<OrderItem> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id).UseIdentityColumn();
+        builder.Property(x => x.ProductId).IsRequired();
+        builder.Property(x => x.ProductName).HasMaxLength(200).IsRequired();
+        builder.Property(x => x.UnitPrice).HasColumnType("decimal(18,2)").IsRequired();
+    }
+}
